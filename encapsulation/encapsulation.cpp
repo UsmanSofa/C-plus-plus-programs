387caa4edb40c5d81f374b4d4b1dd1421bc7283f
UsmanSofa <usmansofawala70@gmail.com>
2026-09-07 09:54:35 +0530

    Encapsulation Program Updated

--- encapsulation/encapsulation.cpp
+++ encapsulation/encapsulation.cpp
@@ -11,73 +11,40 @@ private:
     float per;
 
 public:
-    void setGRID(int g)
-    {
-        grid = g;
+    void setData(){
+        cout << "Enter Your GRID: " << endl;
+        cin >> grid;
+        cout << "Enter Your Name: " << endl;
+        cin >> name;
+        cout << "Enter Your Age: " << endl;
+        cin >> age;
+        cout << "Enter Your Course: " << endl;
+        cin >> course;
+        cout << "Enter Your Percentage: " << endl;
+        cin >> per;
     }
-    void setName(const char n[])
-    {
-        strcpy(name, n);
-    }
-    void setAge(int a)
-    {
-        age = a;
-    }
-    void setCourse( const char c[])
-    {
-        strcpy(course, c);
-    }
-    void setPercentage(float p)
-    {
-        per = p;
-    }
-    int getGRID()
-    {
-        return grid;
-    }
-    char *getName()
-    {
-        return name;
-    }
-    int getAge()
-    {
-        return age;
-    }
-    char *getCourse()
-    {
-        return course;
-    }
-    float getPercentage()
-    {
-        return per;
+    void getData(){
+        cout << "Student GRID: " << grid << endl
+         << "Student Name: " << name << endl
+         << "Student Age: " << age << endl
+         << "Student Course: " << course << endl
+         << "Student Percentage: " << per << endl;
     }
 };
 int main()
 {
-    Student s1,s2;
-    s1.setGRID(101);
-    s1.setName("Usman");
-    s1.setAge(20);
-    s1.setCourse("C++");
-    s1.setPercentage(79.6);
+    int n;
+    cout << "Enter Number of Objects You wanted to create: ";
+    cin >> n;
+    Student students[n];
+    for(int i = 0; i < n;i++){
+        students[i].setData();
+    }
+    cout <<"Students Details" << endl << endl;
 
-    s2.setGRID(102);
-    s2.setName("John Doe");
-    s2.setAge(22);
-    s2.setCourse("Python");
-    s2.setPercentage(65.6);
-
-    cout << "Student GRID: " << s1.getGRID() << endl
-         << "Student Name: " << s1.getName() << endl
-         << "Student Age: " << s1.getAge() << endl
-         << "Student Course: " << s1.getCourse() << endl
-         << "Student Percentage: " << s1.getPercentage() << endl;
-    cout << endl;
-    cout << "Student GRID: " << s2.getGRID() << endl
-         << "Student Name: " << s2.getName() << endl
-         << "Student Age: " << s2.getAge() << endl
-         << "Student Course: " << s2.getCourse() << endl
-         << "Student Percentage: " << s2.getPercentage() << endl;
-    
+    for(int i = 0;i < n;i++){
+        students[i].getData();
+        cout << endl;
+    }
          return 0;
 }
