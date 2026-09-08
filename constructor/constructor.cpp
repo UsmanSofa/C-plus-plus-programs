d9fab52397192215ad6eb7a6d192fcf22343c293
UsmanSofa <usmansofawala70@gmail.com>
2026-09-08 15:14:57 +0530

    Constructor Program Added

---
+++ constructor/constructor.cpp
@@ -0,0 +1,41 @@
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+class Student
+{
+private:
+    int grid;
+    char name[100];
+    int age;
+    char course[100];
+    float per;
+
+public:
+    Student(int g,char n[],int a,char c[],float p){
+        this->grid = g;
+        strcpy(this->name,n);
+        this->age = a;
+        strcpy(this->course,c);
+        this->per = p;
+    }
+    void getData(){
+        cout << "Student GRID: " << grid << endl
+         << "Student Name: " << name << endl
+         << "Student Age: " << age << endl
+         << "Student Course: " << course << endl
+         << "Student Percentage: " << per << endl;
+    }
+};
+
+int main()
+{
+  
+    Student student1(101,"Usman",20,"Python",78.5);
+    Student student2();
+
+    cout << "Students Details: " << endl << endl;
+    student1.getData();
+
+         return 0;
+}
