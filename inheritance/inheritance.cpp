1240b4e26d54f2873da8b3710f97baa0ac013992
UsmanSofa <usmansofawala70@gmail.com>
2026-09-09 16:10:21 +0530

    Inheritance Program Added

---
+++ inheritance/inheritance.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+class Animal{
+    public:
+        char type[100];
+    void eating(){
+        cout << "He is Eating" << endl;
+    }
+
+};
+class Dog :: public Animal{
+    
+};
